de3775dbe716d6742b7f11d9c726e7e5b30dfabe
Aras Pranckevicius <aras@nesnausk.org>
2022-06-16 16:30:21 +0300

    Use DirectXTex decoder

--- libs/sal.h
+++ libs/sal.h
@@ -8,10 +8,16 @@
 
 #define _Out_
 #define _Out_opt_
+#define _Out_writes_(x)
+#define _Out_writes_bytes_(x)
 #define _Out_writes_bytes_to_opt_(x,y)
 
 #define _Inout_updates_bytes_(x)
 #define _Inout_updates_all_(x)
 #define _Inout_updates_all_opt_(x)
 
+#define _Success_(x)
 #define _When_(x,y)
+#define _Use_decl_annotations_
+
+#define _Analysis_assume_(x)
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "dds_loader.h"
-#define BCDEC_IMPLEMENTATION 1
-#include "../libs/bcdec/bcdec.h"
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION 1
 #include "../libs/bcdec/stb_image_write.h"
+
+#define BCDEC_IMPLEMENTATION 1
+#include "../libs/bcdec/bcdec.h"
+
 #define RGBCX_IMPLEMENTATION 1
 #include "../libs/bc7enc/rgbcx.h"
 #include "../libs/bc7enc/bc7decomp.h"
 
+#include "../libs/DirectXTex/DirectXTex/BC.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -116,6 +120,69 @@ static bool decode_bc7dec(int width, int height, unsigned int format, const void
     return true;
 }
 
+static bool decode_dxtex(int width, int height, unsigned int format, const void* input, void* output)
+{
+    using namespace DirectX;
+    const uint8_t* src = (const uint8_t*)input;
+    uint8_t* dst = (uint8_t*)output;
+    XMVECTOR rgbaf[NUM_PIXELS_PER_BLOCK];
+    PackedVector::XMUBYTEN4 rgbai[NUM_PIXELS_PER_BLOCK];
+    for (int i = 0; i < height; i += 4)
+    {
+        for (int j = 0; j < width; j += 4)
+        {
+            if (format == FORMAT_DXT1) {
+                D3DXDecodeBC1(rgbaf, src);
+                src += BCDEC_BC1_BLOCK_SIZE;
+                for (int c = 0; c < 16; ++c) XMStoreUByteN4(rgbai+c, rgbaf[c]);
+                for (int r = 0; r < 4; ++r) memcpy(dst + (i*width+j)*4 + width * 4 * r, rgbai + 4 * r, 4 * 4);
+            } else if (format == FORMAT_DXT3) {
+                D3DXDecodeBC2(rgbaf, src);
+                src += BCDEC_BC2_BLOCK_SIZE;
+                for (int c = 0; c < 16; ++c) XMStoreUByteN4(rgbai+c, rgbaf[c]);
+                for (int r = 0; r < 4; ++r) memcpy(dst + (i*width+j)*4 + width * 4 * r, rgbai + 4 * r, 4 * 4);
+            } else if (format == FORMAT_DXT5) {
+                D3DXDecodeBC3(rgbaf, src);
+                src += BCDEC_BC3_BLOCK_SIZE;
+                for (int c = 0; c < 16; ++c) XMStoreUByteN4(rgbai+c, rgbaf[c]);
+                for (int r = 0; r < 4; ++r) memcpy(dst + (i*width+j)*4 + width * 4 * r, rgbai + 4 * r, 4 * 4);
+            } else if (format == FORMAT_BC7_UNORM) {
+                D3DXDecodeBC7(rgbaf, src);
+                src += BCDEC_BC7_BLOCK_SIZE;
+                for (int c = 0; c < 16; ++c) XMStoreUByteN4(rgbai+c, rgbaf[c]);
+                for (int r = 0; r < 4; ++r) memcpy(dst + (i*width+j)*4 + width * 4 * r, rgbai + 4 * r, 4 * 4);
+            } else if (format == FORMAT_BC4_UNORM) {
+                D3DXDecodeBC4U(rgbaf, src);
+                src += BCDEC_BC4_BLOCK_SIZE;
+                for (int c = 0; c < 16; ++c) {
+                    XMStoreUByteN4(rgbai+c, rgbaf[c]);
+                    dst[(i*width+j) + width*(c/4) + (c&3)] = rgbai[c].x;
+                }
+            } else if (format == FORMAT_BC5_UNORM) {
+                D3DXDecodeBC5U(rgbaf, src);
+                src += BCDEC_BC5_BLOCK_SIZE;
+                for (int c = 0; c < 16; ++c) {
+                    XMStoreUByteN4(rgbai+c, rgbaf[c]);
+                    dst[(i*width+j)*2 + width*2*(c/4) + (c&3)*2 + 0] = rgbai[c].x;
+                    dst[(i*width+j)*2 + width*2*(c/4) + (c&3)*2 + 1] = rgbai[c].y;
+                }
+            } else if (format == FORMAT_BC6H_UF16 || format == FORMAT_BC6H_SF16) {
+                if (format == FORMAT_BC6H_UF16)
+                    D3DXDecodeBC6HU(rgbaf, src);
+                else
+                    D3DXDecodeBC6HS(rgbaf, src);
+                src += BCDEC_BC6H_BLOCK_SIZE;
+                for (int c = 0; c < 16; ++c) {
+                    memcpy(dst + (i*width+j)*12 + width*(c/4)*12 + (c&3)*12, rgbaf+c, 12);
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 typedef bool (DecodeFunc)(int width, int height, unsigned int format, const void* input, void* output);
 
 struct Decoder
@@ -128,6 +195,7 @@ static Decoder s_Decoders[] =
 {
     {"bcdec", decode_bcdec},
     {"bc7dec", decode_bc7dec},
+    {"dxtex", decode_dxtex},
 };
 
 
